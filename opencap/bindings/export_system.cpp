2d187ef84408d8840d71d4c2662ddc9388b3dfa6
James Gayvert <jrg444@gmail.com>
2020-05-28 22:07:04 -0400

    Native types
    Former-commit-id: 1292afa4c1517c3cf03bb041f37284ba679346bd

--- opencap/bindings/export_system.cpp
+++ opencap/bindings/export_system.cpp
@@ -10,6 +10,7 @@ PYBIND11_MODULE(pycap, m) {
 		.def(py::init<>())
         .def("set_geometry", &System::set_geometry)
 		.def("print_dict",&System::print_dict)
+		.def("print_dict2",&System::print_dict2)
 		;
 }
 
