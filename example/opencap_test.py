873cd5fe78cb4a7b6319cf5b0e8fcad6be6a0cf6
James Gayvert <jrg444@gmail.com>
2020-05-29 15:48:57 -0400

    Rearranged params structure a bit
    Former-commit-id: 7f1153a80bd6f29f2cf8cb79af984d4663f73ec4

--- example/opencap_test.py
+++ example/opencap_test.py
@@ -1,11 +1,11 @@
 import pycap
-my_dict = {"geometry":    '''N  0  0   1.039
+sys_dict = {"geometry":    '''N  0  0   1.039
                              N  0  0   -1.039
                             Gh 0  0   0.0''',
             "basis_file":"test_bas.bas",
             "bohr_coordinates": "true",
-            "cart_bf": "",
-            "method" : "ms-caspt2",
+            "cart_bf": ""}
+cap_dict = {"method" : "ms-caspt2",
             "package": "openmolcas",
             "molcas_output":"anion_reference.out",
             "rassi_h5":"test.rassi.h5",
@@ -14,9 +14,10 @@ my_dict = {"geometry":    '''N  0  0   1.039
             "cap_x":"2.76",
             "cap_y":"2.76",
             "cap_z":"4.88",
-            "radial_precision": "14",
-            "angular_points": "110"}
+            "Radial_precision": "14",
+            "angular_points": "110"
+}
 #s = pycap.System()
-s = pycap.System(my_dict)
-pc = pycap.Projected_CAP(s)
+s = pycap.System(sys_dict)
+pc = pycap.Projected_CAP(s,cap_dict)
 pc.run()
