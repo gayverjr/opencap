ca084d7b411a98d98f6c74325c74750aeb01acc6
James Gayvert <jrg444@gmail.com>
2020-08-12 16:57:26 -0400

    fixed some paths

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,7 +5,6 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 	brew install hdf5
 	brew install eigen
 	brew install doxygen
-	brew install cmake
 else
 	sudo apt-get --force-yes install libhdf5-dev
 	sudo apt-get --force-yes install libeigen3-dev
