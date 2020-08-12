2bd38377849aef0bc4573b04000059bd935cde0b
James Gayvert <jrg444@gmail.com>
2020-08-12 18:09:31 -0400

    python versioning

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,14 +4,27 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 	brew update
 	brew install hdf5
 	brew install eigen
-	brew install doxygen
 	brew install gcc@9
 else
+    if [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]
+    then
+    	sudo apt-get --force-yes install python3.8-venv
+    	sudo apt-get --force-yes install python3.8-dev
+    	sudo apt-get --force-yes install python3.8-dev
+    elif [ "$TRAVIS_PYTHON_VERSION" = "3.7" ]
+    then
+    	sudo apt-get --force-yes install python3.7-venv
+    	sudo apt-get --force-yes install python3.7-dev
+    	sudo apt-get --force-yes install python3.7-dev
+    elif [ "$TRAVIS_PYTHON_VERSION" = "3.8" ]
+    then
+    	sudo apt-get --force-yes install python3.8-venv
+    	sudo apt-get --force-yes install python3.8-dev
+    	sudo apt-get --force-yes install python3.8-dev
+    fi
 	sudo apt-get --force-yes install libhdf5-dev
 	sudo apt-get --force-yes install libeigen3-dev
 	sudo apt-get --force-yes install doxygen
 	sudo apt-get --force-yes install lcov
-	sudo apt-get --force-yes install python3.8-venv
-    sudo apt-get --force-yes install python3.8-dev
-    sudo apt-get --force-yes install python3.8-dev
+
 fi
