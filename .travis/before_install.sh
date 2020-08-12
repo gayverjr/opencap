7d16fe31526180b7a337c2281b8ee5aaf5223b47
James Gayvert <jrg444@gmail.com>
2020-08-12 16:24:53 -0400

    Add install

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,8 +7,8 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 	brew install doxygen
 	brew install cmake
 else
-	apt-get libhdf5-dev
-	apt-get libeigen3-dev
-	apt-get doxygen
-	apt-get lcov
+	sudo apt-get --force-yes install libhdf5-dev
+	sudo apt-get --force-yes install libeigen3-dev
+	sudo apt-get --force-yes install doxygen
+	sudo apt-get --force-yes install lcov
 fi
