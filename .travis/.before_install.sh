256ccc09ad0e872a1eadc449099b5462c7fcdcb8
James Gayvert <jrg444@gmail.com>
2020-08-12 15:48:00 -0400

    How about this

---
+++ .travis/.before_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+	brew update;
+	brew install hdf5;
+	brew install eigen;
+	brew install doxygen;
+	brew install cmake;
+    esac
+else
+    sudo apt-get libhdf5-dev;
+    sudo apt-get libeigen3-dev;
+    sudo apt-get doxygen;
+    sudo apt-get lcov;
+fi
