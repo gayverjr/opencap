eee860961e355d2c2d9606a9d901f42e2894d04b
James Gayvert <jrg444@gmail.com>
2020-08-12 19:32:20 -0400

    Let's try this

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-	brew update
 	brew install hdf5
 	brew install eigen
-	brew install gcc@9
+	brew install gcc
 else
 	sudo apt-get --force-yes install libhdf5-dev
 	sudo apt-get --force-yes install libeigen3-dev
