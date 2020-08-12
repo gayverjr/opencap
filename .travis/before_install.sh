40bd1fed348ea4fed8aadc13bd0128f615acdd60
James Gayvert <jrg444@gmail.com>
2020-08-12 19:45:38 -0400

    maybe this will fix macos

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-	brew install hdf5
-	brew install eigen
-	brew install gcc
+	echo $CC
 else
 	sudo apt-get --force-yes install libhdf5-dev
 	sudo apt-get --force-yes install libeigen3-dev
 	sudo apt-get --force-yes install doxygen
 	sudo apt-get --force-yes install lcov
-
 fi
