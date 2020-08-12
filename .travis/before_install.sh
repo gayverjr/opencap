bd690a8e8fa78d87b565ed7b7ebf5248082c3050
James Gayvert <jrg444@gmail.com>
2020-08-12 16:20:24 -0400

    Maybe htis will fix it

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-	brew update;
-	brew install hdf5;
-	brew install eigen;
-	brew install doxygen;
-	brew install cmake;
+	brew update
+	brew install hdf5
+	brew install eigen
+	brew install doxygen
+	brew install cmake
 else
-    sudo apt-get libhdf5-dev;
-    sudo apt-get libeigen3-dev;
-    sudo apt-get doxygen;
-    sudo apt-get lcov;
+	apt-get libhdf5-dev
+	apt-get libeigen3-dev
+	apt-get doxygen
+	apt-get lcov
 fi
