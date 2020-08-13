c922ceebc5c2ec6fbe510026613fb6cf2024a746
James Gayvert <jrg444@gmail.com>
2020-08-13 02:13:47 -0400

    put it back

---
+++ .travis/before_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $TRAVIS_OS_NAME = 'linux' ]; then
+	sudo apt-get --force-yes install libhdf5-dev
+	sudo apt-get --force-yes install libeigen3-dev
+	sudo apt-get --force-yes install doxygen
+	sudo apt-get --force-yes install lcov
+fi 
