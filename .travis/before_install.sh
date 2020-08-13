f15a5c3e572058ddbf0fd02fd7677b235a337ea6
James Gayvert <jrg444@gmail.com>
2020-08-12 22:41:31 -0400

    no this

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-if [ $TRAVIS_OS_NAME = 'linux' ]; then
+
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+	brew install hdf5
+	brew install eigen
+	brew install gcc
+else
 	sudo apt-get --force-yes install libhdf5-dev
 	sudo apt-get --force-yes install libeigen3-dev
 	sudo apt-get --force-yes install doxygen
