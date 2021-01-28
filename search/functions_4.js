c26a5bf0366b16dd861d1f8a3e45a0d44dcd3a37
gayverjr <jrg444@gmail.com>
2021-01-28 16:20:42 +0000

    Deploying to gh-pages from  @ a2b09d876b89d053b6b19b2790653002fc298d63 [formerly 5e917be8d6f760bded006860d922acf8da4ae339] 🚀
    Former-commit-id: 3bb9bcae99f361051be931f4029846229670f5ba

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['element',['element',['../structelement.html#a31f47e23797cdca7b9c9a1ad549dba2f',1,'element']]],
   ['evaluate',['evaluate',['../class_shell.html#af53eca4ca8eff44ce2c032f8d7cd5d8b',1,'Shell']]],
+  ['evaluate_5fon_5fgrid',['evaluate_on_grid',['../class_shell.html#a4ab8e8391807586eb3c52bdc12cb6324',1,'Shell']]],
   ['exception',['Exception',['../class_exception.html#a53bf3b11d41090b81619796fc3a44b2c',1,'Exception']]],
   ['exceptions',['exceptions',['../namespacedate.html#a9c6060496d3b5e21fe23e701d10b3935',1,'date']]],
   ['extract_5fmonth',['extract_month',['../namespacedate_1_1detail.html#a7a6875dc60538c876852d3dc44ec914f',1,'date::detail']]],
