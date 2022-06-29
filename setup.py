747f6616a7d4010dedc3b5d04178678687c826b7
gayverjr <jrg444@gmail.com>
2022-06-28 22:47:51 -0500

    Bump version number.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pyopencap',
-    version='1.2.4',
+    version='1.2.5',
     author='James Gayvert',
     author_email='jrg444@gmail.com',
     description=DOCLINES[0],
